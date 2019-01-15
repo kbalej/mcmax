a49fc67746c3e5d41b9a4a563dcb77493b06c664
Max Balej <42780704+kbalej@users.noreply.github.com>
2019-01-15 09:44:25 +0100

    Add files via upload

--- router.js
+++ router.js
@@ -1,11 +1,8 @@
 ﻿function route(handle, dbh, pathname, querystring, response, postData, request) { 
  if (typeof handle[pathname] === 'function') {
-     handle[pathname](response, postData, pathname, querystring, request, dbh); 
+    handle[pathname](response, postData, pathname, querystring, request, dbh); 
  } else { 
-  //console.log("No request handler found for " + pathname); 
-  //response.writeHead(404, {"Content-Type": "text/plain"}); 
-  //response.write("404 Not found: " + pathname); 
-  //response.end(); 
+    handle["KB_x_showFile"](response, postData, pathname, querystring, request, dbh);
  }
 } 
 exports.route = route;
